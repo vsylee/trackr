e97b849e5c33cd6a40c8d79e42b5f7b66e027983
mcslao <czarina.s.lao@gmail.com>
2016-04-11 23:38:12 -0400

    add event button works with default values

--- js/events.js
+++ js/events.js
@@ -3,8 +3,8 @@
 function addEventDialog(popoverId) {
 	var dialog =
 		'<div class="addEvent-dialog" id="addEvent-dialog-text">' +
-			'<form id="add_event" action="javascript:addEvent(' + popoverId + ');" class="addEvent-form">' +
-				'<label for="name">Event Name: </label><input type="text" name="name" id="name" required autofocus/><br>' +
+			'<form id="add_event" action="javascript:addEvent(\'' + popoverId + '\');" class="addEvent-form">' +
+				'<label for="name">Event Name: </label><input type="text" name="name" id="name" required autofocus=true/><br>' +
 				'<label for="startDate">Start Date: </label><input type="text" name="date" id="startDate" required/><br>' +
 				'<label for="startTime">Start Time: </label><input type="time" name="startTime" id="startTime" required/><br>' +
 				'<label for="endDate">End Date: </label><input type="text" name="date" id="endDate"/><br>' + 
@@ -18,7 +18,7 @@ function addEventDialog(popoverId) {
 					  '<!-- <option value="">generate list of teams</option> -->' +
 					'</select><br>' +
 				'<label for="request">Request Feedback: </label><input type="checkbox" name="request" id="request"/><br>' +
-				'<input type="submit" value="Save and Share"/>' +
+				'<input type="submit" value="Save and Share">' +
 			'</form>' +
 		'</div>'
 	return dialog;
@@ -36,47 +36,72 @@ function addEvent(popoverObj) {
 		start: startDateTime.toISOString(),
 	}
 	$('#calendar').fullCalendar('renderEvent', event);
-	$(popoverObj).css({'display':'none'});
-	popover.remove();
+	$('#'+popoverObj).css({'display':'none'});
+	if (popoverObj !== 'addEvent-button-popover') popover.remove();
 }
 
 function showAddEventPopover(parentObj) {
+	console.log(parentObj.parent().parent().parent().parent().parent()); //fc-day-grid
+	// var parent = parentObj.attr('id') === undefined ? $('#calendar') : parentObj;
+	// parent = $('#addEvent-button');
 	var parent = parentObj;
-	var id = parent.attr('id') + '-popover';
+	console.log(parentObj);
+	console.log(parent);
+	// var id = parent.attr('id') + '-popover';
+	var id = "hi";
 	parent.after('<div id="' + id + '" class="fc-popover">' +
-    					'<div class="fc-header fc-widget-header"> <span class="fc-close fc-icon fc-icon-x"></span><span class="fc-title">Add Event</span><div class="fc-clear"></div>'
+    					'<div class="fc-header fc-widget-header"> <span class="fc-close fc-icon fc-icon-x"></span><span class="fc-title">Event Details</span><div class="fc-clear"></div>'
     					+ '</div>'
     					+ '<div class="fc-body fc-widget-content"><div class="fc-content">' + addEventDialog(id) +'</div>'
     					+ '</div>'
     				+ '</div>');
 	var popover = $('#'+id);
-	var top = parent.position().top + parent.outerHeight();
+	var top = parentObj.position().top + parent.outerHeight();
+	var left = parentObj.position().left + parent.outerWidth();
 
-	popover.css({'display':'block', 'top':top, 'z-index':'1000'});
+	$('#name').focus();
+
+	popover.css({'display':'block', 'top':top, 'z-index':'100000'});
 
 	popover.on('click', '.fc-close', function() {
 		popover.css({'display':'none'});
-		popover.remove();
-	});
+		console.log(id);
+		if (id !== 'addEvent-button-popover') {
+			popover.remove();
+			console.log(true);
+		}
+		//remove popovers on calendar but not from the addEvent button for safety
+	}).draggable();
 
 	$(function() {
-		$( "#startDate" ).datepicker();	
-		$( "#endDate" ).datepicker();			
+		$( "#startDate" ).datepicker({
+			onClose: function( selectedDate ) {
+        		$( "#endDate" ).datepicker( "option", "minDate", selectedDate );
+        		if ($( "#endDate" ).val() === $( "#startDate" ).val()) {
+        			$('#endTime')[0].min = $('#startTime').val();
+        			$('#startTime')[0].max = $('#endTime').val();
+        		}
+      		}
+  		}).datepicker('setDate', new Date());
+
+		$('#endDate').datepicker({
+			onClose: function( selectedDate ) {
+        		$( "#endDate" ).datepicker( "option", "minDate", selectedDate );
+        		if ($( "#endDate" ).val() === $( "#startDate" ).val()) {
+        			$('#endTime')[0].min = $('#startTime').val();
+        			$('#startTime')[0].max = $('#endTime').val();
+        		}
+      		}
+      	}).datepicker('setDate', new Date());
 	});
 
-	$('#startTime').on('click input propertychange paste', function() {
-	    if ($('#endTime').val() === '') {
-	    	$('#endTime').val($('#startTime').val());
+	$('#startTime').on('focusout', function() {
+		
+	    if ($('#endTime').val() === '' && $('#startTime').val() !== '') {
+	    	$('#endTime').val($('#startTime').val())[0].stepUp(60);
 	    }
 	});
 
-	$('#startDate').on('click input propertychange paste', function() {
-	    if ($('#endDate').val() === '') {
-	    	//TODO: this still doesn't work
-	    	console.log($('#endDate').val());
-	    	$('#endDate').val($('#startDate').val());
-	    }
-	});
 	// $(document).on('mousedown', 'documentMousedown');
 
 
