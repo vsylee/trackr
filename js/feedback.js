06fcdc9d8e65ade776227f1259a9e48136e5b449
Jing Lin <Jinglin@mit.edu>
2016-04-13 00:44:14 -0400

    parsing actual data

--- js/feedback.js
+++ js/feedback.js
@@ -5,6 +5,98 @@ var coach_button_id = "coach_button_";
 var table_columns = ["Player name", "Athlete Comment", "Coach feedback"];
 var previous_comment;
 var previous_button;
+
+
+var events = [];
+var fake_comments = [
+	[
+		{
+			"name": "Czarina",
+			"athlete_comment": "I didn't think there was enough recovery time between the weight lifting and the sprints.",
+			"coach_comment": "That's a great point. I'll take that into future consideration."
+		},
+		{
+			"name": "Veronica",
+			"athlete_comment": "I can really feel myself improving.",
+			"coach_comment": "That's great Veronica. You have three more years left too!"
+		},
+		{
+			"name": "Jing",
+			"athlete_comment": "I am really starting to feel the ball as an extension of my body now.",
+			"coach_comment": "That's the way to really understand the sport."
+		},
+		{
+			"name": "Sam",
+			"athlete_comment": "My stamina has improved so much by doing the mile test.",
+			"coach_comment": "Sam your legs look amazing.",
+		}
+	],
+	[
+	{
+		"name": "Czarina",
+		"athlete_comment": "Coach Jing, your practices SUCK",
+		"coach_comment": "Czarina, I don't suck!"
+	},
+	{
+		"name": "Veronica",
+		"athlete_comment": "Coach Jing, this is for practice 2",
+		"coach_comment": "Veronica, it was on march 18"
+	},
+	{
+		"name": "Jing",
+		"athlete_comment": "Coach Jing, nice legs agneognaewoign gsgnao gnaeowg ghaeowg gaoe gewgegewgoe aog ewgoaewg eogew goew geow geaowiga eowigieag oewagewog eg",
+		"coach_comment": "Jing, I love your name"
+	},
+	{
+		"name": "Sam",
+		"athlete_comment": "Coach Jing, nice legs agneognaewoign gsgnao gnaeowg ghaeowg gaoe gewgegewgoe aog ewgoaewg eogew goew geow geaowiga eowigieag oewagewog eg",
+		"coach_comment": "Sam, you're the greatest"
+	}
+	],
+	[
+	{
+		"name": "Czarina",
+		"athlete_comment": "Coach Jing, your practices SUCK",
+		"coach_comment": "Czarina, I don't suck!"
+	},
+	{
+		"name": "Veronica",
+		"athlete_comment": "Coach Jing, you're so old",
+		"coach_comment": "Veronica, you're so young"
+	},
+	{
+		"name": "Jing",
+		"athlete_comment": "Coach Jing, I LOVE YOU!!!!!!! You're the best",
+		"coach_comment": "Jing, I love your name HAHAHA you're great I love you too"
+	},
+	{
+		"name": "Sam",
+		"athlete_comment": "Coach Jing, nice legs agneognaewoign gsgnao gnaeowg ghaeowg gaoe gewgegewgoe aog ewgoaewg eogew goew geow geaowiga eowigieag oewagewog eg",
+		"coach_comment": "Sam, nice legs"
+	}
+	]
+];
+
+function convert_data() {
+	var keys = Object.keys(gameData)
+	for (var i = 0; i < keys.length; i++) {
+		var current_event = {};
+		var current_event_object = gameData[keys[i]];
+		current_event["eventName"] = "Game";
+		current_event["opponent"] = current_event_object.opponent;
+		current_event["location"] = current_event_object.location;
+		current_event["startTime"] = current_event_object.startTime;
+		current_event["endTime"] = current_event_object.endTime;
+		current_event["date"] = current_event_object.date;
+		current_event["comments"] = fake_comments[parseInt(Math.random() * fake_comments.length)];
+
+		events[i] = current_event
+	}
+}
+
+
+
+
 // Function to place caret at end of div from 
 // http://stackoverflow.com/questions/4233265/contenteditable-set-caret-at-the-end-of-the-text-cross-browser
 function placeCaretAtEnd(el) {
@@ -26,12 +118,16 @@ function placeCaretAtEnd(el) {
 }
 
 $(document).ready(function() {
+	convert_data();
 	var counter = 0;
+	console.log(events);
 	for (var i = 0; i < events.length; i++) {
 		var currentEvent = events[i];
 		var desc = currentEvent.eventName 
 					+ " at "
 					+ currentEvent.location 
+					+ " versus " 
+					+ currentEvent.opponent
 					+ ", " 
 					+ currentEvent.startTime 
 					+ " - " 
