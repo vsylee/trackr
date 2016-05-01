e49e4236ef3046c50cc328a38d6ae812c3171c54
Samuel <samjsong@mit.edu>
2016-04-30 20:56:48 -0400

    working on search bar

--- js/feedback_other.js
+++ js/feedback_other.js
@@ -84,17 +84,41 @@ function setup_card(opponent, date, location, start_time, end_time) {
 	return card_to_add;
 }
 
+function searchKeyPress() {
+	console.log("pressing");
+	// $("#team-members").empty();
+	var searchValue = $("#search-bar").val();
+	var len = searchValue.length;
+	// var currentTeam = $("#team-select").val();
+	// var currentMembers = teams[currentTeam];
+	// var matchingMembers = [];
+	// if (currentMembers) {
+	// 	for (var i=0; i < currentMembers.length; i++) {
+	// 		if (currentMembers[i].substr(0,len).toLowerCase() === searchValue.toLowerCase()) {
+	// 			matchingMembers.push(currentMembers[i]);
+	// 		}
+	// 	}
+	// 	for (var i=0; i < matchingMembers.length; i++) {
+	// 		if (i != 0) {
+	// 			$("#team-members").append("<hr>");
+	// 		}
+	// 		$("#team-members").append(
+	// 			"<li>" + matchingMembers[i] + "</li>");
+	// 	}
+	// }
+}
+
 $(document).ready(function() {
 	convert_data();
 	var events_col = $('#feedback_events');
 	for (var i = 0; i < events.length; i++) {
 		var current_event = events[i];
-		$('<div>')
-			.addClass('feedback_title')
-			.appendTo(events_col);
-			// .css({
-			// 	'opacity': i / 10
-			// });
+		// $('<div>')
+		// 	.addClass('feedback_title')
+		// 	.appendTo(events_col);
+		// 	// .css({
+		// 	// 	'opacity': i / 10
+		// 	// });
 		console.log("How many times");
 		var curr_card = setup_card(current_event['opponent'], 
 								   current_event['date'], 
@@ -104,6 +128,27 @@ $(document).ready(function() {
 		
 		curr_card.appendTo(events_col);
 	}
+
+	$("#search-bar").click( function (e) {
+		if ($(this).val().indexOf("🔍 Search by team, location, or date") > -1) {
+			$(this).val("");
+		}
+	});
+	$(document).click( function (e) {
+		// if not search bar, return the "Search" text
+		if (!$(e.target).closest("#search-bar").length &&
+			!$(e.target).is("#search-bar")) {
+			if ($("#search-bar").val() == "") {
+				$("#search-bar").val("🔍 Search by team, location, or date")
+			}
+		}
+		// if not a menu, close menu
+		if (!$(e.target).is(".header_button")) {
+			$('.header_expansion').css('display', "none");
+			$('.header_button').css('background-color', '#3a87ad');
+
+		}
+	});
 
 
 });
